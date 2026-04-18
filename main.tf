5b10d0d35a4593fa697b03fb7626ae126c735f8a
abiola <vboxuser@localhost.localdomain>
2026-04-17 22:32:16 -0400

    updated main tf

--- main.tf
+++ main.tf
@@ -2,20 +2,29 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "bad_bucket" {
-  bucket = "my-insecure-bucket-123456"
+resource "aws_s3_bucket" "secure_bucket" {
+  bucket = "my-secure-bucket-123456"
 
-  acl = "public-read"
+  acl = "private"
 }
 
-resource "aws_security_group" "bad_sg" {
-  name        = "bad_sg"
-  description = "Allow all traffic"
+resource "aws_s3_bucket_public_access_block" "block_public" {
+  bucket = aws_s3_bucket.secure_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_security_group" "secure_sg" {
+  name        = "secure_sg"
+  description = "Allow limited traffic"
 
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["YOUR_IP/32"]
   }
 }
